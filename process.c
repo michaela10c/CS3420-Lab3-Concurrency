08b0a92e07cd99536e0fe65f3c6a5bdbf06e16a9
Rodrigo Taipe <rt363@cornell.edu>
2020-03-09 21:14:28 -0400

    Implemented process_select

--- process.c
+++ process.c
@@ -3,38 +3,40 @@
 #include <MK64F12.h>
 
 struct process_state {
-    unsigned int * stack_pointer;
+    unsigned int* stack_pointer;
 	struct process_state* next_process;
 };
 
-bool
-
 void push_process(process_t* to_push){
+    // if there is nothing to_push
+    if(to_push == NULL){
+        return;
+    }
     // if there is nothing in process_queue, then just add process
     if(process_queue == NULL){
         process_queue = to_push;
     }
     // if there is, then add to the end
     else{
-        process_t *tail = process_queue;
+        process_t* tail = process_queue;
         while(tail->next_process != NULL){
             tail = tail->next_process;
         }
         tail->next_process = to_push;
     }
-    toAdd->next_process = NULL;
+    to_push->next_process = NULL;
 }
 
-process_t* pop_process(){
+void pop_process(process_t* to_pop){
     .// if empty, return NULL
     if(process_queue == NULL){
-        return NULL;
+        to_pop = NULL;
     }
     // if there is, pop from the front 
     else{
-        process_t* to_pop = process_queue;
-        process_queue = process_queue->next_process;
-        return to_pop;
+        to_pop = process_queue;
+        process_queue = process_queue->next_process; 
+        to_pop->next_process = NULL;
     }
 }
 
@@ -48,7 +50,7 @@ int process_create (void (*f)(void), int n){
     }
     else{
         // allocate space for new_process and push it on to process_queue
-        process_t * new_process = malloc(sizeof(process_t));
+        process_t* new_process = malloc(sizeof(process_t));
         new_process->stack_pointer = new_stack_pointer;
         push_process(new_process);
         return 0;
@@ -57,7 +59,8 @@ int process_create (void (*f)(void), int n){
 
 void process_start (void){
     // set current_process to NULL
-    current_process = NULL;
+    process_queue = NULL;
+    current_process = process_queue;
 
     // setup interrupt
     SIM->SCGC6 = SIM_SCGC6_PIT_MASK;    // Enable clock to PIT module
@@ -65,13 +68,31 @@ void process_start (void){
     PIT->CHANNEL[0].LDVAL = 2250000;    // Set load value of zeroth PIT to 2.25 million for 1 second counts
     PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK;  // enables Timer 0 interrupts
     PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK;  // start Timer 0
-    NVIC_EnableIRQ(PIT0_IRQHandler);    // Enable PIT0 Interrupts
+    NVIC_EnableIRQ(PIT0_IRQn);    // Enable PIT0 Interrupts
 
     // call process_begin()
     process_begin();
 }
 
-unsigned int * process_select(unsigned int * cursp){
-    // TODO: implement process_select
-    return NULL:
+unsigned int * process_select(unsigned int* cursp){
+    // if there is a current_process, pop it from queue
+    if(current_process != NULL){
+        process_t* temp = NULL;
+        pop_process(temp);
+        // if the stack pointer is not NULL, then process isn't done
+        if(cursp != NULL){
+            // so push it back on process_queue
+            push_process(temp);
+
+        else{
+            // otherwise free the space
+            free(temp);
+        }
+    }
+    
+    // retrieve next process
+    current_process = process_queue;
+
+    // if its empty, then return NULL, otherwise return stack pointer
+    return current_process == NULL ? NULL : current_process->stack_pointer;
 }
