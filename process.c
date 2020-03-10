81b41134886111c87f43819f07c0ad30af0eb3f7
Rodrigo Taipe <rt363@cornell.edu>
2020-03-09 20:46:06 -0400

    Implement process_start

--- process.c
+++ process.c
@@ -7,12 +7,14 @@ struct process_state {
 	struct process_state* next_process;
 };
 
+bool
+
 void push_process(process_t* to_push){
-    // if there is nothing in queue, then queue pointer points to process pointer
+    // if there is nothing in process_queue, then just add process
     if(process_queue == NULL){
         process_queue = to_push;
     }
-    // if there is, then queue's tail points to process pointer
+    // if there is, then add to the end
     else{
         process_t *tail = process_queue;
         while(tail->next_process != NULL){
@@ -24,11 +26,11 @@ void push_process(process_t* to_push){
 }
 
 process_t* pop_process(){
-    .// if there is nothing to pop return NULL
+    .// if empty, return NULL
     if(process_queue == NULL){
         return NULL;
     }
-    // if there, pop from the front 
+    // if there is, pop from the front 
     else{
         process_t* to_pop = process_queue;
         process_queue = process_queue->next_process;
@@ -45,10 +47,31 @@ int process_create (void (*f)(void), int n){
         return -1;
     }
     else{
-        // allocate space for a process and push it on to process queue
+        // allocate space for new_process and push it on to process_queue
         process_t * new_process = malloc(sizeof(process_t));
         new_process->stack_pointer = new_stack_pointer;
         push_process(new_process);
         return 0;
     }
+}
+
+void process_start (void){
+    // set current_process to NULL
+    current_process = NULL;
+
+    // setup interrupt
+    SIM->SCGC6 = SIM_SCGC6_PIT_MASK;    // Enable clock to PIT module
+    PIT->MCR &= ~PIT_MCR_MDIS_MASK;     // Disables standard PIT timers
+    PIT->CHANNEL[0].LDVAL = 2250000;    // Set load value of zeroth PIT to 2.25 million for 1 second counts
+    PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK;  // enables Timer 0 interrupts
+    PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK;  // start Timer 0
+    NVIC_EnableIRQ(PIT0_IRQHandler);    // Enable PIT0 Interrupts
+
+    // call process_begin()
+    process_begin();
+}
+
+unsigned int * process_select(unsigned int * cursp){
+    // TODO: implement process_select
+    return NULL:
 }
